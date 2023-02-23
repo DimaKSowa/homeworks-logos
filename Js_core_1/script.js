13c5d8962a6f469a60e2efbb4326983f304bd2dd
DimaK <dimakozlov15102007@gmail.com>
2023-02-23 17:02:38 +0200

    Js core hw 1

---
+++ Js_core_1/script.js
@@ -0,0 +1,36 @@
+// завдання 1
+
+// let abc = prompt("text");
+// console.log(abc.split(" ").length - 1)
+
+// завдання 2
+
+// let mail = prompt("email");
+// let a = mail.indexOf("@");
+// let b = mail.length;
+// var c = b - a;
+// switch (c){
+//     case b:
+//     case b+1:
+//     case 1:
+//         console.log("false");
+//         break
+//     default:
+//         console.log("true")        
+// }
+
+// завдання 3
+
+// let txt = prompt("text");
+// console.log(txt.split("html").length - 1);
+
+// завдання 4
+
+// let a = prompt("adress")
+// if (a.includes('https://')){
+//     console.log(a.replace('https://', ''))
+// }else if (a.includes('http://')){
+//     console.log(a.replace('http://', ''))
+// }else{
+//     console.log("not an adress");
+// }
---
+++ Js_lesson_3/script.js
@@ -0,0 +1,9 @@
+// завдання 2
+// for(let i = 1000; i<=9999; i +=3){
+//     console.log(i)
+// }
+
+// завдання 4
+for(let d = 90; d>0; d - 5){
+    console.log(d)
+}
