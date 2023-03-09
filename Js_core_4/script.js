1e9563fdde54990b79c3182df01cec68d58733ae
DimaK <dimakozlov15102007@gmail.com>
2023-03-09 17:35:19 +0200

    HW-4: added

---
+++ Js_core_4/script.js
@@ -0,0 +1,29 @@
+function backColor() {
+    const background = document.querySelector('body');
+    background.classList.toggle('colorback')
+}
+
+function fontsPage() {
+    const background = document.querySelector('body');
+    background.classList.toggle('fonts')
+}
+
+function logoCentre() {
+    const background = document.querySelector('h1');
+    background.classList.toggle('logo')
+}
+
+function backSites() {
+    const background = document.querySelector('.sites');
+    background.classList.toggle('backsites')
+}
+
+function colorSites() {
+    const background = document.querySelector('.sites');
+    background.classList.toggle('text_color')
+}
+
+function divText() {
+    const background = document.querySelector('.text');
+    background.classList.toggle('div_text')
+}
